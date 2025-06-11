7d1ff2bcff74d330fb8db620b08d39f69c4fc7c1
Pratik Singh <pratikkumars623@gmail.com>
2025-06-11 11:37:24 +0530

    Basic setup and nativewind configuration

---
+++ nativewind-env.d.ts
@@ -0,0 +1,1 @@
+///<referenece types="nativewind/types" />
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   presets: [require("nativewind/preset")],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C7FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
+        accent: "#AB8BFF",
+      },
+    },
+  },
+  plugins: [],
+};
